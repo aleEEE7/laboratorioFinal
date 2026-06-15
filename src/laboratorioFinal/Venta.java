d8ff3e4148102c15e8975129521cc6b2ffe6961b
Rodrigo Alonso <rodrigoalonsomartinezjuearez@gmail.com>
2026-06-15 16:30:51 -0600

    terminada clase venta y detalle

---
+++ src/laboratorioFinal/Venta.java
@@ -0,0 +1,56 @@
+package laboratorioFinal;
+
+public class Venta {
+
+    private int idVenta;
+    private String fecha;
+    private Cliente cliente;
+    private Empleado empleado;
+
+    public Venta(int idVenta, String fecha, Cliente cliente, Empleado empleado) {
+        this.idVenta = idVenta;
+        this.fecha = fecha;
+        this.cliente = cliente;
+        this.empleado = empleado;
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta [idVenta=" + idVenta +
+                ", fecha=" + fecha +
+                ", cliente=" + cliente +
+                ", empleado=" + empleado + "]";
+    }
+}
