cf18aee9bdbd92156b0058b943b8b50f2e66f906
aleEEE7 <alexisflores3389@gmail.com>
2026-06-15 16:00:20 -0600

    Agregada clase categoria y clase producto

---
+++ src/laboratorioFinal/Producto.java
@@ -0,0 +1,86 @@
+package laboratorioFinal;
+
+public class Producto {
+
+    private int idProducto;
+    private String nombre;
+    private double precioCompra;
+    private double precioVenta;
+    private int cantidadMax;
+    private int cantidadMin;
+    private Categoria categoria;
+
+    public Producto(int id, String nombre, double pc,
+                    double pv, Categoria cat) {
+        this.idProducto = id;
+        this.nombre = nombre;
+        this.precioCompra = pc;
+        this.precioVenta = pv;
+        this.categoria = cat;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int id) {
+        this.idProducto = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nom) {
+        this.nombre = nom;
+    }
+
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(double precio) {
+        this.precioCompra = precio;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precio) {
+        this.precioVenta = precio;
+    }
+
+    public int getCantidadMax() {
+        return cantidadMax;
+    }
+
+    public void setCantidadMax(int cant) {
+        this.cantidadMax = cant;
+    }
+
+    public int getCantidadMin() {
+        return cantidadMin;
+    }
+
+    public void setCantidadMin(int cant) {
+        this.cantidadMin = cant;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria cat) {
+        this.categoria = cat;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [idProducto=" + idProducto +
+                ", nombre=" + nombre +
+                ", precioCompra=" + precioCompra +
+                ", precioVenta=" + precioVenta +
+                ", categoria=" + categoria + "]";
+    }
+}
